3531665bb2ef5f9c9651ff4354c79776c543dfcc
Chiranth7954 <ckishore@purdue.edu>
2017-05-24 15:27:23 -0400

    Convert Thing from a stateless functional component to class from class

--- src/Thing.js
+++ src/Thing.js
@@ -1,28 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ContentEditable from 'react-contenteditable'
 
 import './Thing.css'
 import RemoveThing from './RemoveThing'
 
-const Thing = ({ thing, saveThing, removeThing }) => {
-  const updateName = (ev) => {
+class Thing extends Component {
+  updateName = (ev) => {
+    const { thing, saveThing} = this.props
     thing.name = ev.target.value
     saveThing(thing)
   }
 
-  return (
-    <li className="Thing">
-      <input type="checkbox" value="on" />
-      <div className="details">
-        <ContentEditable
-          className="name"
-          html={thing.name}
-          onChange={updateName}
-        />
-        <RemoveThing thing={thing} removeThing={removeThing}/>
-      </div>
-    </li>
-  )
+  render() {
+    const { thing, removeThing } = this.props
+
+    return (
+      <li className="Thing">
+        <input type="checkbox" value="on" />
+        <div className="details">
+          <ContentEditable
+            className="name"
+            html={thing.name}
+            onChange={this.updateName}
+          />
+          <RemoveThing
+            thing={thing}
+            removeThing={removeThing}
+          />
+        </div>
+      </li>
+    )
+  }
 }
 
 export default Thing
