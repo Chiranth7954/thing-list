73f595777ec8809ebc5b0e51961ed1e4d9caf58e
Chiranth7954 <ckishore@purdue.edu>
2017-05-25 14:29:18 -0400

    Add sign out component and button from class

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import './App.css'
 import Header from './Header'
+import SignOut from './SignOut'
 import AddThing from './AddThing'
 import ThingList from './ThingList'
 import base from './base'
@@ -58,6 +59,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <SignOut />
         <AddThing addThing={this.addThing} />
         <ThingList
           things={this.state.things}
---
+++ src/SignOut.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './SignOut.css'
+
+const SignOut = () => {
+  return (
+    <button
+      className="SignOut"
+    >
+      Sign Out
+    </button>
+  )
+}
+
+export default SignOut
